d1348bea4fce034e48007da2a8a32c0ffb9925a5
William4650 <williamphenicie@outlook.com>
2020-05-20 15:15:59 -0500

    Initial Commit

---
+++ Drink Journal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Drink Journal
+//
+//  Created by William Phenicie on 5/20/20.
+//  Copyright © 2020 Peer Group Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
